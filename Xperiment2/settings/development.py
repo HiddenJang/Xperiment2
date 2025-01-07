5d2b875942ecf78a04ecd1b84ebf351925876b32
Jang <jangljungl@jmail.com>
2025-01-07 22:32:23 +0400

    change settings

--- Xperiment2/settings/development.py
+++ Xperiment2/settings/development.py
@@ -2,18 +2,18 @@ from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS = ['*']
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'xperiment2db',
-        'USER': 'root',
-        'PASSWORD': 'dontuseit',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'xperiment2db',
+#         'USER': 'root',
+#         'PASSWORD': 'dontuseit',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         }
+#     }
+# }
 
 
